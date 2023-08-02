ad6471ac5662fa7e6e6a84f23422e0628b3e0eee
IshaniGodhani <ishugodhani08@gmail.com>
2023-08-02 10:53:19 +0530

    third commit

--- app/src/main/java/com/example/firebase_ecommerce/ui/Addproduct/AddProductFragment.java
+++ app/src/main/java/com/example/firebase_ecommerce/ui/Addproduct/AddProductFragment.java
@@ -1,16 +1,15 @@
 package com.example.firebase_ecommerce.ui.Addproduct;
 
-import static android.app.Activity.RESULT_OK;
-
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +18,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.firebase_ecommerce.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Random;
 //import com.google.firebase.database.DatabaseReference;
 //import com.google.firebase.database.FirebaseDatabase;
 
@@ -31,9 +38,13 @@ public class AddProductFragment extends Fragment {
     Button submitbutton;
     FirebaseDatabase database;
     DatabaseReference myRef;
+    FirebaseStorage mainBuket;
+    StorageReference imgFolder;
+    UploadTask uploadTask;
+
 
     int SELECT_PICTURE = 200;
-
+    private Object CropImage;
 
 
     @Override
@@ -49,12 +60,67 @@ public class AddProductFragment extends Fragment {
         // Inflate the layout for this fragment
 
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
-        Log.d("TTT", "onCreateView: ooo"+myRef);
+//        myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
+//        Log.d("TTT", "onCreateView: ooo"+myRef);
+        mainBuket = FirebaseStorage.getInstance();
+        String imgName = "Img"+new Random().nextInt(100000)+".jpg";
+        imgFolder = mainBuket.getReference().child("Images/"+imgName);
+        
+        submitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImg();
+            }
+        });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // getImage();
+            }
+        });
 
         return inflater.inflate(R.layout.fragment_add_product, container, false);
     }
 
+//    private void getImage() {
+//        CropImage.activity()
+//                .start(getContext(), this);
+//    }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+//            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+//            if (resultCode == RESULT_OK) {
+//                Uri resultUri = result.getUri();
+//            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+//                Exception error = result.getError();
+//            }
+//        }
+//    }
+    private void uploadImg() {
+        imageView.setDrawingCacheEnabled(true);
+        imageView.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        uploadTask = imgFolder.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+            }
+        });
     }
+
+}
